583c4744764294b98471ebceb484d4a3b67af357
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-11-15 20:12:33 +0900

    コンポーネント削除

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head';
 import { Footer } from '../components/Footer/Footer';
-import { Nav } from '../components/Nav/Nav';
+import Link from 'next/link';
+import InfoIcon from '@material-ui/icons/Info';
+import WorkIcon from '@material-ui/icons/Work';
+import ContactMailIcon from '@material-ui/icons/ContactMail';
 
 export default function Home() {
   return (
@@ -9,7 +12,34 @@ export default function Home() {
         <title>Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Nav />
+      <nav
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          marginTop: '30%',
+        }}
+      >
+        <Link href="/about">
+          <a>
+            <InfoIcon style={{ fontSize: 80, marginRight: 32 }}></InfoIcon>
+            <p>About</p>
+          </a>
+        </Link>
+        <Link href="/work">
+          <a>
+            <WorkIcon style={{ fontSize: 80, marginRight: 32 }}></WorkIcon>
+            <p>Works</p>
+          </a>
+        </Link>
+        <Link href="/contact">
+          <a>
+            <ContactMailIcon
+              style={{ fontSize: 80, marginRight: 32 }}
+            ></ContactMailIcon>
+            <p>Contact</p>
+          </a>
+        </Link>
+      </nav>
       <Footer />
     </>
   );
